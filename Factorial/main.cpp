283ff66c7bb92c5719b7b0867218e3c87eeae40e
Aj Gill <ajgill@ucdavis.edu>
2018-09-22 14:19:10 -0700

    Factorial
    Recursive Factorial for a hard coded value.
    Next will be user input and memoization.

--- Factorial/main.cpp
+++ Factorial/main.cpp
@@ -7,9 +7,18 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+int factorial(int num){
+    if (num == 0){
+        return 1;
+    }
+    else{
+        return num * factorial(num - 1);
+    }
+}
+
+int main() {
+    int total = factorial(5);
+    cout << total << endl;
 }
